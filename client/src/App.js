025a0c51f2d69e6b766c47202ec9c4a2ddd79bac
ceylon85 <canleez123@gmail.com>
2020-06-28 23:54:40 +0900

    chat-bot

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Typography, Icon } from 'antd';
-import Chatbot from './Chatbot/Chatbot';
+
 const { Title } = Typography;
 
 function App() {
@@ -10,8 +10,6 @@ function App() {
         <Title level={2} >CHAT BOT APP&nbsp;<Icon type="robot" /></Title>
       </div>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-       
-        <Chatbot />
 
 
       </div>
